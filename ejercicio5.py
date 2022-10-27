c0d1f1016f4525faa7e64aeadf847d380984fe65
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-27 10:18:40 +0000

    ejercicio 5 casi completo

--- ejercicio5.py
+++ ejercicio5.py
@@ -7,12 +7,13 @@ resto= num%1000
 centena= (resto-(resto%100)) / 100
 resto = num%100
 decena= (resto-(resto%10)) / 10
-unidad = resto % 10
+unidad = resto % 10 
 print("\n")
 
 print("UNIDADES:",'%04d' % unidad)
 print("DECENAS:", '%03d' % decena)
 print("CENTENAS:",'%02d' % centena)
 print("MIL:",'%01d' % mil)
+
 
 
