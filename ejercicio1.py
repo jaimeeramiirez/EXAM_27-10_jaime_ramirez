3a0bf998807c47a6da6ebce86578d35a6f379faf
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-27 08:47:59 +0000

    ejercicio 2 term,inado

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,5 +6,28 @@ def mayusculaminuscula():
   var_1_minuscula=var_1.lower()
   print(var_1_mayuscula)
   print(var_1_minuscula)
+  division= len(var_1)/7
+  print(round(division,4))
 
 mayusculaminuscula()
+
+print("\n\n")
+def funcion_1():
+  operación= 12-(4*2)-(2+2)
+  print(operación)
+funcion_1()
+
+def funcion_2():
+  operacion= 12 - 4 * (2 - 2) + 2
+  print(operacion)
+funcion_2()
+
+print("\n\n")
+def edad():
+  edad=int(input("Introduce tu edad: "))
+  if edad >= 18:
+    print("Eres mayor de edad")
+  else:
+    print("Eres menor de edad")
+edad()
+  
--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-#EJERCICIO 1
-
-def mayusculaminuscula():
-  var_1 = "Módulo 1 de Python"
-  var_1_mayuscula= var_1.upper()
-  var_1_minuscula=var_1.lower()
-  print(var_1_mayuscula)
-  print(var_1_minuscula)
-  division= len(var_1)/7
-  print(round(division,4))
-
-mayusculaminuscula()
+#EJERCICIO 2
 
 print("\n\n")
-def funcion_1():
-  operación= 12-(4*2)-(2+2)
-  print(operación)
-funcion_1()
 
-def funcion_2():
-  operacion= 12 - 4 * (2 - 2) + 2
-  print(operacion)
-funcion_2()
+cadena = "zeréP nauJ,01"
 
-print("\n\n")
-def edad():
-  edad=int(input("Introduce tu edad: "))
-  if edad >= 18:
-    print("Eres mayor de edad")
-  else:
-    print("Eres menor de edad")
-edad()
-  
+cadena_invertida= cadena[::-1].split( ",")
+
+print( cadena_invertida[1] + " ha sacado un " + cadena_invertida[0] + " de nota")
