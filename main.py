a09351795e703664a642e00437cb1a6a1c4a713c
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-27 08:44:37 +0000

    ejercicio 1 terminado entero

--- main.py
+++ main.py
@@ -8,7 +8,26 @@ def mayusculaminuscula():
   print(var_1_minuscula)
   division= len(var_1)/7
   print(round(division,4))
-  
-  
 
 mayusculaminuscula()
+
+print("\n\n")
+def funcion_1():
+  operación= 12-(4*2)-(2+2)
+  print(operación)
+funcion_1()
+
+def funcion_2():
+  operacion= 12 - 4 * (2 - 2) + 2
+  print(operacion)
+funcion_2()
+
+print("\n\n")
+def edad():
+  edad=int(input("Introduce tu edad: "))
+  if edad >= 18:
+    print("Eres mayor de edad")
+  else:
+    print("Eres menor de edad")
+edad()
+  
