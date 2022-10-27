164ce431772e66368490f441a82a9f91789e407f
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-27 10:26:34 +0000

    modificado ejercicio 7

--- ejercicio7.py
+++ ejercicio7.py
@@ -5,11 +5,12 @@ listado= [18, 50, 210, 80, 145, 333, 70, 30]
 #El problema está en que, en el bucle for no consigo hacer referencia a los numero por lo que las operaciones que escribo van a salir incorrectas. 
 
 def main():
-  if listado % 10==0:
-    print(listado)
-  else:pass
-
-    
+  for numero in listado:
+    if numero in listado %10==0 && numero<200:
+      print(numero)
+    elif numero in listado >300:
+      print("ERROR")
+  
 if __name__ == '__main__':
   main()
 
